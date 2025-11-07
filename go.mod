50df9645cb438f1989e9b81dab9448390501bcf1
cuong@techmaster.vn <cuong@techmaster.vn>
2025-11-07 15:24:38 +0700

    Logrus

--- go.mod
+++ go.mod
@@ -4,9 +4,7 @@ go 1.25.3
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.9
-	github.com/rs/zerolog v1.34.0
 	github.com/sirupsen/logrus v1.9.3
-	go.uber.org/zap v1.27.0
 )
 
 require (
@@ -17,9 +15,9 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,44 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
-	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/rs/zerolog"
 	"github.com/sirupsen/logrus"
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 // ============================================================================
-// Global Logger Instances
+// Global Logger Instance
 // ============================================================================
 var (
-	slogLogger    *slog.Logger
-	logrusLogger  *logrus.Logger
-	zapLogger     *zap.Logger
-	zerologLogger zerolog.Logger
+	logrusLogger *logrus.Logger
 )
 
-// init khởi tạo tất cả logger instances
+// init khởi tạo logrus logger
 func init() {
-	initSlog()
 	initLogrus()
-	initZap()
-	initZerolog()
-}
-
-// initSlog khởi tạo slog logger
-func initSlog() {
-	opts := &slog.HandlerOptions{
-		Level:     slog.LevelDebug,
-		AddSource: true, // Thêm thông tin về file, line, function
-	}
-	handler := slog.NewJSONHandler(os.Stdout, opts)
-	slogLogger = slog.New(handler)
 }
 
 // initLogrus khởi tạo logrus logger
@@ -53,44 +32,9 @@ func initLogrus() {
 	logrusLogger.SetLevel(logrus.DebugLevel)
 }
 
-// initZap khởi tạo zap logger
-func initZap() {
-	config := zap.NewDevelopmentConfig()
-	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
-	config.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
-
-	var err error
-	zapLogger, err = config.Build(zap.AddCallerSkip(0), zap.AddStacktrace(zapcore.ErrorLevel))
-	if err != nil {
-		panic(fmt.Sprintf("Failed to initialize zap logger: %v", err))
-	}
-}
-
-// initZerolog khởi tạo zerolog logger
-func initZerolog() {
-	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
-	output.FormatLevel = func(i interface{}) string {
-		return fmt.Sprintf("| %-6s|", i)
-	}
-	output.FormatMessage = func(i interface{}) string {
-		return fmt.Sprintf("***%s****", i)
-	}
-	output.FormatFieldName = func(i interface{}) string {
-		return fmt.Sprintf("%s:", i)
-	}
-	output.FormatFieldValue = func(i interface{}) string {
-		return fmt.Sprintf("%s", i)
-	}
-
-	zerologLogger = zerolog.New(output).With().Timestamp().Caller().Logger()
-}
-
 func main() {
-	// Đảm bảo flush zap logger khi thoát
-	defer zapLogger.Sync()
 	app := fiber.New(fiber.Config{
-		AppName: "LearnFiber - Logging Libraries Demo",
+		AppName: "LearnFiber - Logrus Demo",
 	})
 
 	// Middleware
@@ -98,12 +42,9 @@ func main() {
 
 	// Routes
 	app.Get("/", homeHandler)
-	app.Get("/slog", slogHandler)
 	app.Get("/logrus", logrusHandler)
 	app.Get("/logrus2", logrus2Handler)
 	app.Get("/logrus3", logrus3Handler)
-	app.Get("/zap", zapHandler)
-	app.Get("/zerolog", zerologHandler)
 
 	// Start server
 	fmt.Println("Server starting on http://localhost:8081")
@@ -114,44 +55,13 @@ func main() {
 
 func homeHandler(c *fiber.Ctx) error {
 	return c.JSON(fiber.Map{
-		"message": "Welcome to LearnFiber - Logging Libraries Demo",
+		"message": "Welcome to LearnFiber - Logrus Demo",
 		"endpoints": []string{
-			"/slog - Demo log/slog",
 			"/logrus - Demo sirupsen/logrus (chia cho 0)",
 			"/logrus2 - Demo sirupsen/logrus (index out of range)",
 			"/logrus3 - Demo sirupsen/logrus (deep call stack: X->Y->Z->GetElement)",
-			"/zap - Demo uber-go/zap",
-			"/zerolog - Demo rs/zerolog",
 		},
 	})
-}
-
-// ============================================================================
-// log/slog Handler
-// ============================================================================
-func slogHandler(c *fiber.Ctx) error {
-	requestPath := fmt.Sprintf("%s %s", c.Method(), c.Path())
-
-	// Defer function để bắt panic từ chia cho 0
-	defer func() {
-		if r := recover(); r != nil {
-			// Lấy thông tin CHÍNH XÁC về dòng gây panic
-			file, line, funcName := getPanicLocationForHandler("slogHandler")
-
-			// Log panic với thông tin tối giản
-			slogLogger.Error("PANIC: Lỗi chia cho 0 đã xảy ra!",
-				slog.String("request_path", requestPath),
-				slog.String("function", funcName),
-				slog.String("file", fmt.Sprintf("%s:%d", file, line)),
-			)
-		}
-	}()
-
-	// Dòng này sẽ gây panic!
-	denominator := 0
-	result := 100 / denominator
-
-	return c.JSON(fiber.Map{"result": result})
 }
 
 // ============================================================================
@@ -269,60 +179,4 @@ func callZ() int {
 // callZ gọi GetElement
 func callW() int {
 	return GetElement() // Panic sẽ xảy ra trong GetElement, không phải ở đây!
-}
-
-// ============================================================================
-// uber-go/zap Handler
-// ============================================================================
-func zapHandler(c *fiber.Ctx) error {
-	requestPath := fmt.Sprintf("%s %s", c.Method(), c.Path())
-
-	// Defer function để bắt panic từ chia cho 0
-	defer func() {
-		if r := recover(); r != nil {
-			// Lấy thông tin CHÍNH XÁC về dòng gây panic
-			file, line, funcName := getPanicLocationForHandler("zapHandler")
-
-			// Log panic với thông tin tối giản
-			zapLogger.Error("PANIC: Lỗi chia cho 0 đã xảy ra!",
-				zap.String("request_path", requestPath),
-				zap.String("function", funcName),
-				zap.String("file", fmt.Sprintf("%s:%d", file, line)),
-			)
-		}
-	}()
-
-	// Dòng này sẽ gây panic!
-	denominator := 0
-	result := 100 / denominator
-
-	return c.JSON(fiber.Map{"result": result})
-}
-
-// ============================================================================
-// rs/zerolog Handler
-// ============================================================================
-func zerologHandler(c *fiber.Ctx) error {
-	requestPath := fmt.Sprintf("%s %s", c.Method(), c.Path())
-
-	// Defer function để bắt panic từ chia cho 0
-	defer func() {
-		if r := recover(); r != nil {
-			// Lấy thông tin CHÍNH XÁC về dòng gây panic
-			file, line, funcName := getPanicLocationForHandler("zerologHandler")
-
-			// Log panic với thông tin tối giản
-			zerologLogger.Error().
-				Str("request_path", requestPath).
-				Str("function", funcName).
-				Str("file", fmt.Sprintf("%s:%d", file, line)).
-				Msg("PANIC: Lỗi chia cho 0 đã xảy ra!")
-		}
-	}()
-
-	// Dòng này sẽ gây panic!
-	denominator := 0
-	result := 100 / denominator
-
-	return c.JSON(fiber.Map{"result": result})
 }
